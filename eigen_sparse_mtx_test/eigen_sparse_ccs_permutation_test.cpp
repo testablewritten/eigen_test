f34522ed7197b290c90d5b94179347f5b55df991
Jeeseop Kim <jeeseop@vt.edu>
2019-11-07 11:14:07 -0500

    matrix size test code update

--- eigen_sparse_mtx_test/eigen_sparse_ccs_permutation_test.cpp
+++ eigen_sparse_mtx_test/eigen_sparse_ccs_permutation_test.cpp
@@ -15,8 +15,11 @@ int main(){
 
     std::cout << matdense <<std::endl;
 
+    // convert dense mtx to sparse mtx
     mat = matdense.sparseView();
+    //////////////////////////////////
 
+    // how to generate permutation
     AMDOrdering<int> ordering;
     PermutationMatrix<Dynamic, Dynamic, int> perm;
     //Fill the matrix ...
@@ -29,12 +32,30 @@ int main(){
     int *ml;
     ml = a.data(); // change the vector format indices as a array pointer
     std::cout << ml[0]<< ml[1]<< ml[2]<< ml[3] << std::endl;
+    //////////////////////////////////
 
+
+    // generate sparse mtx with CCS format
     mat.makeCompressed();
     
     std::cout << mat.valuePtr()[0] << mat.valuePtr()[1] << mat.valuePtr()[2] << std::endl;
     std::cout << mat.innerIndexPtr()[0] << mat.innerIndexPtr()[1] << mat.innerIndexPtr()[2] << std::endl;
     std::cout << mat.outerIndexPtr()[0] << mat.outerIndexPtr()[1] << mat.outerIndexPtr()[2] << mat.outerIndexPtr()[3] << std::endl;
+    //////////////////////////////////
+
+    // test size function
+    Eigen::Matrix<double, 4, 5> matdense_sizetest;
+    matdense_sizetest << 3,1,2,4,5,1,4,0,0,5,2,0,2,0,5,4,0,0,7,5;
+    int sizecheck_inner;
+    int sizecheck_outer;
+    sizecheck_inner = matdense_sizetest.innerSize();
+    sizecheck_outer = matdense_sizetest.outerSize();
+    std::cout << sizecheck_inner <<std::endl; // innersize gives row number
+    std::cout << sizecheck_outer <<std::endl; // outersize gives column number
+    //////////////////////////////////
+
+
+
 
     return 0;
 }
